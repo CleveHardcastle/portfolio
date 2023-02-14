9edc104a9ca1e602ee889666986ceac3fde63be4
CleveHardcastle <treyhardcastle@gmail.com>
2023-02-13 17:28:23 -0800

    github repo link/new project

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -29,6 +29,11 @@ function Work() {
                 A restful api project that generates recipe text/video walk
                 through for recipes.
               </Typography>
+              <Typography className="text-blue-200">
+                <a href="https://github.com/CleveHardcastle/Recipe_Project">
+                  GitHub Repo
+                </a>
+              </Typography>
             </CardBody>
             <CardFooter
               divider
@@ -55,6 +60,11 @@ function Work() {
                 A Handlebars application that allows users to share interests
                 and chat about those interests.
               </Typography>
+              <Typography className="text-blue-200">
+                <a href="https://github.com/CleveHardcastle/friend_finder">
+                  GitHub Repo
+                </a>
+              </Typography>
             </CardBody>
             <CardFooter
               divider
@@ -79,6 +89,11 @@ function Work() {
               </Typography>
               <Typography className="text-white">
                 A React application that showcases all the work I've done.
+              </Typography>
+              <Typography className="text-blue-200">
+                <a href="https://github.com/CleveHardcastle/portfolio">
+                  GitHub Repo
+                </a>
               </Typography>
             </CardBody>
             <CardFooter
@@ -107,6 +122,42 @@ function Work() {
               <Typography className="text-white">
                 A React application that allows people to find shelters near
                 them and discuss cat breeds.
+              </Typography>
+              <Typography className="text-blue-200">
+                <a href="https://github.com/talexander91/Purrfect_Pals">
+                  GitHub Repo
+                </a>
+              </Typography>
+            </CardBody>
+            <CardFooter
+              divider
+              className="flex items-center justify-between py-3"
+            ></CardFooter>
+          </Card>
+        </div>
+        <div className="md:w-1/3 m-3 md:self-center">
+          <Card className="w-full">
+            <CardHeader color="blue" className="relative h-56 md:m-4">
+              <a href="https://coalition-skill-test.vercel.app/">
+                <img
+                  src={process.env.PUBLIC_URL + "/images/skilltest.jpg"}
+                  alt="img-blur-shadow"
+                  className="h-full w-full border"
+                />
+              </a>
+            </CardHeader>
+            <CardBody className="text-center">
+              <Typography variant="h5" className="mb-2 text-white">
+                Coalition Skill Test
+              </Typography>
+              <Typography className="text-white">
+                A React application to showcase my ability to make designs based
+                off documentation.
+              </Typography>
+              <Typography className="text-blue-200">
+                <a href="https://github.com/CleveHardcastle/CoalitionSkillTest">
+                  GitHub Repo
+                </a>
               </Typography>
             </CardBody>
             <CardFooter
